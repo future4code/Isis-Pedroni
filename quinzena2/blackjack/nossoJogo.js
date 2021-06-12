40249186ba3ebdbf01a7e676e911a5a7d1747edf
Isis Pedroni <isispedroni3@gmail.com>
2021-06-12 13:16:55 -0300

    Alteração 0.60

--- quinzena2/blackjack/nossoJogo.js
+++ quinzena2/blackjack/nossoJogo.js
@@ -14,24 +14,35 @@
   //        PROJETO!
 
 
-console.log("Bem vindo(a) ao jogo de Blackjack!")
+  console.log("Bem vindo(a) ao jogo de Blackjack!")
 
-let jogar = confirm("Quer iniciar uma nova rodada?")
-if(confirm("pergunta de sim ou não")) {
-
-   let carta1Jogador = comprarCarta()
-   let carta2Jogador = comprarCarta()
-   let carta1Pc = comprarCarta()
-   let carta2Pc = comprarCarta()
-
-
-   let somaCartaJogador = 
-   let somaCartaPc = 
-
-
-
-
-} else {
-	return "O jogo acabou"
-}
-
+  let jogar = confirm("Quer iniciar uma nova rodada?")
+  
+  
+  if (jogar) {
+  
+     let carta1Jogador = comprarCarta()
+     let carta2Jogador = comprarCarta()
+     let carta1Pc = comprarCarta()
+     let carta2Pc = comprarCarta()
+  
+     let somarCartaJogador = carta1Jogador.valor + carta2Jogador.valor
+     let somarCartaPc = carta1Pc.valor + carta2Pc.valor
+  
+     console.log(`Cartas Jogador: ${carta1Jogador.texto} ${carta2Jogador.texto} ${somarCartaJogador}`)
+     console.log(`Cartas Pc: ${carta1Pc.texto} ${carta2Pc.texto} ${somarCartaPc}`)
+  
+  if (somarCartaJogador === somarCartaPc) {
+     console.log("Empate!")
+  
+  } else if (somarCartaJogador > somarCartaPc) {
+     console.log("O usuário ganhou!")
+  
+  } else if (somarCartaJogador < somarCartaPc) {
+     console.log("O computador ganhou!")
+  }
+  
+  } else {
+     console.log("O jogo acabou")
+  }
+  
