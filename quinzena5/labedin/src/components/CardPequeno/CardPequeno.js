dbd7372a4d3bfd673704c1d5be7e12c1829a3b4d
Isis Pedroni <isispedroni3@gmail.com>
2021-07-29 20:19:02 -0300

    atualização~]

--- quinzena5/labedin/src/components/CardPequeno/CardPequeno.js
+++ quinzena5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,5 +1,24 @@
-import React from "react"
-import "./CardPequeno.css"
+import React from "react";
+import "./CardPequeno.css";
+// import styled from "styled-components";
+
+
+// const CardGeral = styled.div `
+//     display: flex;
+//     align-items: center;
+//     border: 1px solid black;
+//     padding: 20px 10px;
+//     margin-bottom: 10px;
+//     height: 200px;
+//     `;
+
+// const ImgCard= styled.img `
+//     width: 70px;
+//     margin-right: 10px;
+//     border-radius: 50%;
+//     `;
+
+
 
 const CardPequeno = (props) => {
 
