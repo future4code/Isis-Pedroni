e3b6ed3001573d2e1e7f32023656c7fe35387264
Isis Pedroni <isispedroni3@gmail.com>
2021-07-30 22:14:52 -0300

    Terminado

--- quinzena5/labedin/src/components/CardGrande/CardGrande.js
+++ quinzena5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import './CardGrande.css'
+import React from "react";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+  height: 200px;
+`;
+
+const CardImage = styled.img`
+  width: 70px;
+  margin-right: 10px;
+`;
+
+const TextH = styled.h4`
+  margin-bottom: 4px;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 function CardGrande(props) {
-    return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
-    )
+  return (
+    <CardContainer>
+      <CardImage src={props.imagem} />
+      <ContentContainer>
+        <TextH>{props.nome}</TextH>
+        <p>{props.descricao}</p>
+      </ContentContainer>
+    </CardContainer>
+  );
 }
 
 export default CardGrande;
+
