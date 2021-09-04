8446552acdcad12fb2c3c2dcb95ebe4eaf568d76
Isis Pedroni <isispedroni3@gmail.com>
2021-09-03 21:58:43 -0300

    Iniciando

---
+++ astromatch/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HomePage } from './pages/HomePage';
+import { MatchesPage } from './pages/MatchesPage';
+
+const App = () => {
+
+//aqui faço a renderição AULA DE SEGUNDA STATES
+
+
+  return (
+    <div>
+      <HomePage/>
+      <MatchesPage/>
+      ola
+    </div>
+  );
+}
+
+export default App;
